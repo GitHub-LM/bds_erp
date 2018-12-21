a1a9fadd4b930aed1af70c1388d9e6fc5f1f9c34
GitHub-LM <785601077@qq.com>
2018-12-21 13:47:47 +0800

    初始化上传代码

---
+++ src/utils/validate.js
@@ -0,0 +1,40 @@
+export function isvalidUsername(str) {
+  const valid_map = ['admin', 'test']
+  return valid_map.indexOf(str.trim()) >= 0
+}
+
+/* 合法uri*/
+export function validateURL(textval) {
+  const urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/
+  return urlregex.test(textval)
+}
+
+/* 小写字母*/
+export function validateLowerCase(str) {
+  const reg = /^[a-z]+$/
+  return reg.test(str)
+}
+
+/* 大写字母*/
+export function validateUpperCase(str) {
+  const reg = /^[A-Z]+$/
+  return reg.test(str)
+}
+
+/* 大小写字母*/
+export function validatAlphabets(str) {
+  const reg = /^[A-Za-z]+$/
+  return reg.test(str)
+}
+
+/* 合法手机号 */
+export function validPhone(str){
+  const reg=/(^([0\*][0-9\*]{2,3}\s)?([2-9\*][0-9\*]{6,7})+(\-[0-9\*]{1,4})?$)|(^([0\*][0-9\*]{2,3})?([2-9\*][0-9\*]{6,7})+(\-[0-9\*]{1,4})?$)|(^([0\*][0-9\*]{2,3}[\-\*])?([2-9\*][0-9\*]{6,7})+(\-[0-9\*]{1,4})?$)|(^((\(\d{3}\))|(\d{3}\-))?([1\*][34578\*][0-9\*]{9})$)/;
+  return reg.test(str)
+}
+
+/* 邮政编码*/
+export function validPostcode(str) {
+  const reg =/^[0-9][0-9]{5}$/;
+  return reg.test(str)
+}
---
+++ src/views/tms/one/one.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>我是测试1
+         <table border="2">
+        <tr>
+            <td>第三个</td>
+            <td>第三个</td>
+            <td>第三个</td>
+            <td>第三个</td>
+
+        </tr>
+         <tr>
+            <td>第三个</td>
+            <td>第三个</td>
+            <td>第三个</td>
+            <td>第三个</td>
+
+        </tr>
+         <tr>
+            <td>第三个</td>
+            <td>第三个</td>
+            <td>第三个</td>
+            <td>第三个</td>
+
+        </tr>
+
+    </table>
+    </div>
+   
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
